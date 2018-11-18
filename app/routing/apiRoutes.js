c52ee13c7aa9c958d9f549819e7ffd6fbb3c0737
Jrew <jrewmohamed@gmail.com>
2018-11-17 21:05:40 -0500

    Working on api functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,33 @@ module.exports = function (app) {
     })
 
     app.post('/api/friends', function (req, res) {
-        friends.push(req.body);
-        res.json(true);
+
+        const newFriend = req.body;
+        const userResponses = newFriend.scores;
+
+        let friendName = '';
+        let friendImage = '';
+        let totalDifference = 10000;
+
+        for (let i = 0; i < friends.length; i++) {
+
+            let difference = 0;
+            for (let j = 0; j < userResponses.length; j++) {
+                difference += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+
+            if (difference < totalDifference) {
+
+                totalDifference = difference;
+                friendName = friends[i].name;
+                friendImage = friends[i].photo;
+            }
+        }
+
+        friends.push(newFriend);
+
+        res.json({status: 'OK', friendName: friendName, friendImage: friendImage});
+
     })
 
 }
